c48a8bfe9b73161365493b10c28194fc7ca14385
ddd <daiman4444@gmail.com>
2024-01-26 18:45:12 +0600

    changes for running bot from main.py

--- team_bot/bot.py
+++ team_bot/bot.py
@@ -1,8 +1,5 @@
-import asyncio
-import logging
-
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.fsm.strategy import FSMStrategy
 
 from team_bot.config import config
@@ -11,7 +8,7 @@ from team_bot.handlers import messages
 
 async def main():
     bot = Bot(token=config.TOKEN)
-    storage = MemoryStorage()
+    storage = RedisStorage.from_url(config.REDIS_URL.unicode_string(), )
     dp = Dispatcher(storage=storage, fsm_strategy=FSMStrategy.USER_IN_CHAT)
     
     # подключение роутеров
@@ -19,9 +16,3 @@ async def main():
     
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-    
-    
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
